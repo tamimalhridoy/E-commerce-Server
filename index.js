9510e13fc94c38dc057f92af2dcc0ba638fe511f
Tamim Al Hridoy <162357537+tamimalhridoy@users.noreply.github.com>
2025-07-15 23:56:17 +0600

    start project

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-console.log("hello");
+// require("dotenv").config();
+// const express = require("express");
+// const dbConfig = require("./dbConfig/dbConnect");
+// const router = require("./router");
+// const cors = require("cors");
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
 
-console.log("wold");
+// dbConfig();
+
+// app.use(router);
+
+// app.listen(5000, () => console.log("Server is running"));
